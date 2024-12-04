5f009d617a03aee9b1d0704792a10b84a5f6332d
Shimadakunn <lcombaret@hotmail.co.jp>
2024-12-04 20:44:05 +0100

    Refactor code to simplify account creation process

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,19 @@
 "use client";
 import { useMe } from "@/providers";
-import { useEffect } from "react";
 
 export default function Home() {
   const { create, me } = useMe();
 
-  useEffect(() => {
-    window.addEventListener("message", async (event) => {
-      if (event.data === "CREATE_ACCOUNT") {
-        alert("creating account");
-        await create("leo");
-        alert("account created");
-        window.ReactNativeWebView?.postMessage(
-          JSON.stringify({
-            type: "ACCOUNT_CREATED",
-            data: me,
-          })
-        );
-        alert("sent message");
-      }
-    });
-  }, [create, me]);
+  const connect = () => {
+    create("leo");
+    const appRedirectUrl = `kuma://home?address=${me?.account}`;
+    window.location.href = appRedirectUrl;
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>Hello World</h1>
-      <button onClick={() => create("leo")}>Connect</button>
+      <button onClick={connect}>Connect</button>
     </div>
   );
 }
